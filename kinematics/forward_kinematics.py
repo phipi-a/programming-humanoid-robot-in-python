d7dd64d766c0609fa7257598d6514bbbe32b7399
Philipp <phpi@gmx.de>
2022-06-02 22:08:23 +0200

    2-kinematics

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -17,32 +17,94 @@
 '''
 
 # add PYTHONPATH
+
+
+from cmath import pi
+from math import atan2
+from numpy.matlib import matrix, identity
+import numpy as np
+from numpy import cos, sin, arcsin
 import os
 import sys
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
+sys.path.append(os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), '..', 'joint_control'))
+sys.path.append(os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), '..', 'software_installation'))
+from spark_agent import INVERSED_JOINTS
 
-from numpy.matlib import matrix, identity
 
+from keyframes.hello import hello
 from recognize_posture import PostureRecognitionAgent
 
+ 
 
+def from_trans(m):
+    return [m[0, -1], m[1, -1], m[2, -1], atan2(m[2,1],m[2,2]), atan2(-m[2,0],np.sqrt(m[2,1]**2+m[2,2]**2)), atan2(m[1,0],m[0,0])]
+
+
+def rad2deg(x):
+    return x*180/np.pi
+def deg2rad(x):
+    return x*np.pi/180
 class ForwardKinematicsAgent(PostureRecognitionAgent):
     def __init__(self, simspark_ip='localhost',
                  simspark_port=3100,
                  teamname='DAInamite',
                  player_id=0,
                  sync_mode=True):
-        super(ForwardKinematicsAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        super(ForwardKinematicsAgent, self).__init__(
+            simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
-                       # YOUR CODE HERE
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch'], 
+                        'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll'], 
+                        'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
+                        'RArm': ['RShoulderPitch', 'RShoulderRoll','RElbowYaw', 'RElbowRoll' ], 
+                        'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll']
+                       
                        }
+        
 
+        self.rotation_axis = {'HeadYaw': ('z',0,0,126,-2.0857,2.0857), 'HeadPitch': ('y',0,0,0,-0.6720, 0.5149), 
+                            'RShoulderPitch': ('y',0,-98,100,-2.0857,2.0857),'RShoulderRoll': ('z',0,0,0,-1.3265,0.3142), 'RElbowYaw': ('x',105,-15,0,-2.0857, 2.0857), 'RElbowRoll': ('z',0,0,0,0.0349, 1.5446), 
+                            'LShoulderPitch': ('y',0,98,100,-2.0857,2.0857),'LShoulderRoll': ('z',0,0,0,-0.3142,1.3265), 'LElbowYaw': ('x',105,-15,0,-2.0857, 2.0857), 'LElbowRoll': ('z',0,0,0,0.0349, 1.5446), 
+                            'LHipYawPitch': ('y,z',0,50,-85,-1.145303, 0.740810), 'LHipRoll': ('x',0,0,0,	-0.379472, 0.790477), 'LHipPitch': ('y',0,0,0,-1.535889, 0.484090), 'LKneePitch': ('y',0,0,-100,-0.092346, 2.112528), 'LAnklePitch': ('y',0,0,-102.9,-1.189516, 0.922747), 'LAnkleRoll': ('x',0,0,0,-0.397880, 0.769001),
+                            'RHipYawPitch': ('y,z',0,-50,-85,-1.145303, 0.740810), 'RHipRoll': ('x',0,0,0,	-0.790477, 0.379472), 'RHipPitch': ('y',0,0,0,-1.535889, 0.484090), 'RKneePitch': ('y',0,0,-100,-0.103083, 2.120198), 'RAnklePitch': ('y',0,0,-102.9,-1.186448, 0.932056), 'RAnkleRoll': ('x',0,0,0,-0.768992, 0.397935)
+                            }
+
+        self.i=0
+    
     def think(self, perception):
+        
         self.forward_kinematics(perception.joint)
+        if self.i%100==0:
+            np.set_printoptions(suppress=True)
+            values=[np.round(from_trans(self.transforms[z]),decimals=2) for z in self.chains["LLeg"]]
+            for i,_ in enumerate(values):
+                values[i]=values[i][:3]
+            print(*list(zip( self.chains["LLeg"],values)),sep='')
+            print("\n")
+        self.i+=1
         return super(ForwardKinematicsAgent, self).think(perception)
+
+    def getRotationMatrix(self, axis, angle,x,y,z):
+        n = np.identity(4)
+        r = angle
+        if axis == 'x':
+            n[0:3, 0:3] = np.asarray(
+                [[1, 0, 0], [0, cos(r), -sin(r)], [0, sin(r), cos(r)]])
+        elif axis == 'y':
+            n[0:3, 0:3] = np.asarray(
+                [[cos(r), 0, sin(r)], [0, 1, 0], [-sin(r), 0, cos(r)]])
+        elif axis == 'z':
+            n[0:3, 0:3] = np.asarray(
+                [[cos(r), -sin(r), 0], [sin(r), cos(r), 0], [0, 0, 1]])
+        n[:-1,-1]=[x,y,z]
+        return n
+
+    
+
 
     def local_trans(self, joint_name, joint_angle):
         '''calculate local transformation of one joint
@@ -53,9 +115,27 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         :rtype: 4x4 matrix
         '''
         T = identity(4)
-        # YOUR CODE HERE
+        motion_axis,x,y,z,min,max = self.rotation_axis[joint_name]
+        joint_angle=np.clip(joint_angle,min,max)
+
+        if motion_axis == 'y,z':
+            T = self.getRotationMatrix('x', deg2rad(45),0,0,0)
+            T = T@self.getRotationMatrix('y', joint_angle,x,y,z)
+            T = T@self.getRotationMatrix('x', deg2rad(-45),0,0,0)
+        else:
+            T = self.getRotationMatrix(motion_axis, joint_angle,x,y,z)
+
 
         return T
+
+    def forward_kinematics_own(self,effector_name,joints):
+        T = identity(4)
+        for joint in self.chains[effector_name]:
+            angle = joints[joint]
+            Tl = self.local_trans(joint,angle)
+            T = T@Tl
+        return T
+
 
     def forward_kinematics(self, joints):
         '''forward kinematics
@@ -63,14 +143,20 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         :param joints: {joint_name: joint_angle}
         '''
         for chain_joints in self.chains.values():
+
             T = identity(4)
             for joint in chain_joints:
                 angle = joints[joint]
-                Tl = self.local_trans(joint, angle)
-                # YOUR CODE HERE
-
+                Tl = self.local_trans(joint,angle)
+                T = T@Tl
                 self.transforms[joint] = T
+
 
 if __name__ == '__main__':
     agent = ForwardKinematicsAgent()
+    a=(["RShoulderRoll"],
+    [[1,2]],
+    [[[0, [],[]], [-1, [],[]]]])
+
+    agent.keyframes = a
     agent.run()
--- kinematics/inverse_kinematics.py
+++ kinematics/inverse_kinematics.py
@@ -10,12 +10,40 @@
 '''
 
 
+from ntpath import join
 from forward_kinematics import ForwardKinematicsAgent
 from numpy.matlib import identity
+import numpy as np
+from numpy import arcsin, arccos, linalg,arctan2
+from scipy.optimize import fmin, minimize
 
+def from_trans(m):
+    return np.asarray([m[0, -1], m[1, -1], m[2, -1], arctan2(m[2,1],m[2,2]), arctan2(-m[2,0],np.sqrt(m[2,1]**2+m[2,2]**2)), arctan2(m[1,0],m[0,0])])
 
+def translate_rad(x):
+    if x <0:
+        x=(-1*x)%(2*np.pi)*-1
+    else:
+        x=x%(2*np.pi)
+    if x>np.pi:
+        x=(2*np.pi-x)*-1
+    if x<-np.pi:
+        x=x-2*np.pi*-1
+    return x
+def rad2deg(x):
+    return np.round(x*180/np.pi,decimals=1)
+def deg2rad(x):
+    return x*np.pi/180
 class InverseKinematicsAgent(ForwardKinematicsAgent):
-    def inverse_kinematics(self, effector_name, transform):
+
+    
+    def error_func(self,joint_angles,joint_names, target,effector_name):
+        Ts=self.forward_kinematics_own(effector_name,dict(zip(joint_names, joint_angles)))
+        e = from_trans(target)[:3] - from_trans(Ts)[:3]
+        self.a.append(linalg.norm(e))
+        return linalg.norm(e)
+
+    def inverse_kinematics(self, effector_name, transform,):
         '''solve the inverse kinematics
 
         :param str effector_name: name of end effector, e.g. LLeg, RLeg
@@ -23,20 +51,58 @@ class InverseKinematicsAgent(ForwardKinematicsAgent):
         :return: list of joint angles
         '''
         joint_angles = []
-        # YOUR CODE HERE
-        return joint_angles
 
-    def set_transforms(self, effector_name, transform):
+        target = transform
+        if effector_name=="test":
+            joint_names=self.chains_test[effector_name]
+        else:
+            joint_names=self.chains[effector_name]
+
+        joint_names=joint_names
+
+        joint_angles= [0]*len(joint_names)
+        func = lambda t: self.error_func(t,joint_names, target,effector_name)
+        
+        #res=minimize(func, joint_angles, method='SLSQP')
+        #joint_angles=res.x
+        joint_angles= fmin(func, joint_angles)
+
+        for i,n in enumerate(joint_names):
+            #TODO clip angle
+            _,_,_,_,min,max=self.rotation_axis[n]
+            joint_angles[i]=np.clip(translate_rad(joint_angles[i]),min,max)
+
+        joint_angles_dict = dict(zip(joint_names, joint_angles))
+        
+
+        # YOUR CODE HERE
+        return joint_angles_dict
+
+    def set_transforms(self, effector_name, transform,start_time):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
-        self.keyframes = ([], [], [])  # the result joint angles have to fill in
+        self.a=[]
+        X=self.inverse_kinematics(effector_name,transform)
+        print(X)
+        print("Final Position:",from_trans(self.forward_kinematics_own(effector_name,X)))
+        names=[]
+        times=[]
+        keys=[]
+        for k,v in X.items():
+            names.append(k)
+            times.append([start_time,start_time+1])
+            keys.append([[0.1,[0,0,0], [0,0,0]],[v,[0,0,0], [0,0,0]]])
+        self.keyframes = (names, times, keys)  # the result joint angles have to fill in
 
 if __name__ == '__main__':
     agent = InverseKinematicsAgent()
+    np.set_printoptions(suppress=True)
     # test inverse kinematics
-    T = identity(4)
-    T[-1, 1] = 0.05
-    T[-1, 2] = -0.26
-    agent.set_transforms('LLeg', T)
+    T = np.identity(4)
+    T[:-1,-1]= np.asarray([0, 0.05, -0.26])*1000
+    
+    print("Target Position:",from_trans(T))
+    agent.set_transforms('LLeg', T,0)
+    
     agent.run()
